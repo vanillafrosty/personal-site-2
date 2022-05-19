ea8a39b5170dc1c12bf72bfd25607903cc4265ef
David Wu <dwu.inq@gmail.com>
2022-05-19 14:09:58 -0400

    revamp how images are loaded. use dynamic urls in public folder instead of loading all images at once. also reduce image size with squoosh.

--- src/assets/images/markers/1/index.js
+++
@@ -1,4 +0,0 @@
-import First from "./cantina-mayahuel-1.jpg";
-import Second from "./cantina-mayahuel-2.jpg";
-
-export { First, Second };
--- src/assets/images/markers/3/index.js
+++
@@ -1,5 +0,0 @@
-import First from "./the-cottage-1.jpg";
-import Second from "./the-cottage-2.jpg";
-import Third from "./the-cottage-3.jpg";
-
-export { First, Second, Third };
