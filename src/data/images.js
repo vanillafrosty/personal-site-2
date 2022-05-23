97122e1240f43b2f8f16a502095da37294c6fe50
David Wu <dwu.inq@gmail.com>
2022-05-23 19:05:00 -0400

    add more restaurants and images

--- src/data/images.js
+++ src/data/images.js
@@ -13,6 +13,42 @@ const data = {
     "/assets/images/3/the-cottage-2-min.jpg",
     "/assets/images/3/the-cottage-3-min.jpg",
   ],
+  4: [
+    "/assets/images/4/calis-and-fries-1-min.jpg",
+    "/assets/images/4/calis-and-fries-2-min.jpg",
+    "/assets/images/4/calis-and-fries-3-min.jpg",
+  ],
+  5: ["/assets/images/5/toast-1-min.jpg", "/assets/images/5/toast-2-min.jpg"],
+  6: [
+    "/assets/images/6/maggies-cafe-1-min.jpg",
+    "/assets/images/6/maggies-cafe-2-min.jpg",
+    "/assets/images/6/maggies-cafe-3-min.jpg",
+  ],
+  7: [
+    "/assets/images/7/crack-shack-1-min.jpg",
+    "/assets/images/7/crack-shack-2-min.jpg",
+    "/assets/images/7/crack-shack-3-min.jpg",
+  ],
+  8: [
+    "/assets/images/8/juniper-and-ivy-1-min.jpg",
+    "/assets/images/8/juniper-and-ivy-2-min.jpg",
+    "/assets/images/8/juniper-and-ivy-3-min.jpg",
+  ],
+  9: [
+    "/assets/images/9/mochinut-1-min.jpg",
+    "/assets/images/9/mochinut-2-min.jpg",
+    "/assets/images/9/mochinut-3-min.jpg",
+  ],
+  10: [
+    "/assets/images/10/trust-1-min.jpg",
+    "/assets/images/10/trust-2-min.jpg",
+    "/assets/images/10/trust-3-min.jpg",
+  ],
+  11: [
+    "/assets/images/11/bear-buns-bakery-1-min.jpg",
+    "/assets/images/11/bear-buns-bakery-2-min.jpg",
+    "/assets/images/11/bear-buns-bakery-3-min.jpg",
+  ],
 };
 
 export { data };
